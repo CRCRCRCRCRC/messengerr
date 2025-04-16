66d0227699b9f9817acc1bf96b160449ee17fdbd
CRCRCRCRCRC <corrty12345678901234567890@gmail.com>
2025-04-16 22:16:05 +0800

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -4,8 +4,8 @@ const messageSchema = new mongoose.Schema({
   from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   message: { type: String, required: true },
-  isRead: { type: Boolean, default: false },
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
+  read: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
